12d64a72b6cfbc787c47498fe93f4e5bf573646f
Dmitriifd <idimka9@gmail.com>
2022-09-29 15:02:22 +0500

    расчет ипотеки

--- js/model.js
+++ js/model.js
@@ -101,8 +101,24 @@ function setData(newData) {
 		...newData,
 	}
 
+	// Рассчет ипотеки
+	const months = data.time * 12
+
+	const totalAmount = data.cost - data.payment
+
+	const monthRate = data.selectedProgram / 12
+
+	const generalRate = (1 + monthRate) ** months
+
+	const monthPayment = (totalAmount * monthRate * generalRate) / (generalRate - 1)
+
+	const overPayment = monthPayment * months - totalAmount
+
 	results = {
 		rate: data.selectedProgram,
+		totalAmount,
+		monthPayment,
+		overPayment,
 	}
 }
 
